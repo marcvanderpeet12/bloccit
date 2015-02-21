ab6a1559c16e187a7e7bc24caa51d3a72d88705e
Marc van der Peet <marcvanderpeet@gmail.com>
2015-02-21 20:25:10 +0000

    Topics and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,16 +12,28 @@
  end
  users = User.all
 
+  # Create Topics
+ 15.times do
+   Topic.create!(
+     name:         Faker::Lorem.sentence,
+     description:  Faker::Lorem.paragraph
+   )
+ end
+ topics = Topic.all
+
 
  # Create Posts
  50.times do
    Post.create!(
      user:   users.sample,
+     topic:  topics.sample,
      title:  Faker::Lorem.sentence,
      body:   Faker::Lorem.paragraph
    )
  end
  posts = Post.all
+
+
  
  # Create Comments
  100.times do
@@ -32,12 +44,35 @@
  end
 
 
- user = User.first
- user.skip_reconfirmation!
- user.update_attributes!(
-   email: 'marcvanderpeet@gmail.com',
+ # Create an admin user
+ admin = User.new(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ admin.skip_confirmation!
+ admin.save!
+ 
+ # Create a moderator
+ moderator = User.new(
+   name:     'Moderator User',
+   email:    'moderator@example.com',
+   password: 'helloworld',
+   role:     'moderator'
+ )
+ moderator.skip_confirmation!
+ moderator.save!
+ 
+ # Create a member
+ member = User.new(
+   name:     'Member User',
+   email:    'member@example.com',
    password: 'helloworld'
  )
+ member.skip_confirmation!
+ member.save!
+
  
  puts "Seed finished"
  puts "#{Post.count} posts created"
