527f449d1c2fb8732c08219dbe3bab90d1ac56ff
Marc van der Peet <marcvanderpeet@gmail.com>
2015-02-22 09:27:55 +0000

    Changed post- and topics-params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,7 @@ class PostsController < ApplicationController
 
   def create
     @topic = Topic.find(params[:topic_id])
-    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    @post = current_user.posts.build(post_params)
     authorize @post
     if @post.save
       flash[:notice] = "Post was saved."
@@ -38,7 +38,7 @@ class PostsController < ApplicationController
      @topic = Topic.find(params[:topic_id])
      @post = Post.find(params[:id])
      authorize @post
-     if @post.update_attributes(params.require(:post).permit(:title, :body))
+     if @post.update_attributes(post_params)
        flash[:notice] = "Post was updated."
        #deze werkt nog niet!
        redirect_to @post
@@ -47,4 +47,10 @@ class PostsController < ApplicationController
        render :edit
      end
    end
+end
+
+private
+
+def post_params
+  params.require(:post).permit(:title, :body)
 end
