9955506b3de25abba0dc8d86173e2a92a5ce435f
Marc van der Peet <marcvanderpeet@gmail.com>
2015-02-24 10:07:53 +0000

    Active users

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :topics do
-     resources :posts, except: [:index]
+     resources :posts, except: [:index], controller: 'topics/posts'
    end
  
-   resources :posts, only: [] do
+   resources :posts, only: [:index] do
      resources :comments, only: [:create, :destroy]
      resources :favorites, only: [:create, :destroy]
 
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
      post '/down-vote' => 'votes#down_vote', as: :down_vote
    end
   
-  resources :users, only: [:update, :show]
+  resources :users, only: [:update, :show, :index]
   
   get 'about' => 'welcome#about'
 
