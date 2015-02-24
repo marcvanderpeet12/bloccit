318006a88151a4b16be23731e87c33d23bd192c7
Marc van der Peet <marcvanderpeet@gmail.com>
2015-02-24 09:41:18 +0000

    Public profiles

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
      post '/down-vote' => 'votes#down_vote', as: :down_vote
    end
   
-  resources :users, only: [:update]
+  resources :users, only: [:update, :show]
   
   get 'about' => 'welcome#about'
 
